f3e4cc8484ed9e2016eaf69e1dc2cfafa3b3a220
Владимир <svr93@i.ua>
2015-07-07 06:21:54 +0400

    add_pics_links

--- development/js/app.js
+++ development/js/app.js
@@ -17,16 +17,27 @@
   var contentElems = document.getElementsByClassName('content');
   data.people = new PeopleDataContainer(header, contentElems[0]);
 
-  var jsonData = '[["Иван",10,10,["v.gd/a","v.gd/b","v.gd/c","v.gd/d"]],\
-                   ["Игорь",11,20,["v.gd/a","v.gd/b","v.gd/c"]],\
-                   ["Станислав",12,30,["v.gd/a","v.gd/b","v.gd/c"]],\
-                   ["Степан",13,40,["v.gd/a","v.gd/b","v.gd/c"]],\
-                   ["Глеб",14,50,["v.gd/a","v.gd/b","v.gd/c"]],\
-                   ["Ульяна",15,60,["v.gd/a","v.gd/b","v.gd/c"]],\
-                   ["Григорий",16,70,["v.gd/a","v.gd/b","v.gd/c"]],\
-                   ["Ирина",17,80,["v.gd/a","v.gd/b","v.gd/c"]],\
-                   ["Татьяна",18,90,["v.gd/a","v.gd/b","v.gd/c"]],\
-                   ["Светлана",19,100,["v.gd/a","v.gd/b","v.gd/c"]]]';
+  var jsonData =
+    '[["\/\/upload.wikimedia.org\/wikipedia\/ru\/c\/c9\/Tom_the_Cat.jpg",\
+       "Иван",10,10,["v.gd/a","v.gd/b","v.gd/c","v.gd/d"]],\
+      ["\/\/upload.wikimedia.org\/wikipedia\/ru\/c\/c9\/Tom_the_Cat.jpg",\
+       "Игорь",11,20,["v.gd/a","v.gd/b","v.gd/c"]],\
+      ["\/\/upload.wikimedia.org\/wikipedia\/ru\/c\/c9\/Tom_the_Cat.jpg",\
+       "Станислав",12,30,["v.gd/a","v.gd/b","v.gd/c"]],\
+      ["\/\/upload.wikimedia.org\/wikipedia\/ru\/c\/c9\/Tom_the_Cat.jpg",\
+       "Степан",13,40,["v.gd/a","v.gd/b","v.gd/c"]],\
+      ["\/\/upload.wikimedia.org\/wikipedia\/ru\/c\/c9\/Tom_the_Cat.jpg",\
+       "Глеб",14,50,["v.gd/a","v.gd/b","v.gd/c"]],\
+      ["\/\/upload.wikimedia.org\/wikipedia\/ru\/c\/c9\/Tom_the_Cat.jpg",\
+       "Ульяна",15,60,["v.gd/a","v.gd/b","v.gd/c"]],\
+      ["\/\/upload.wikimedia.org\/wikipedia\/ru\/c\/c9\/Tom_the_Cat.jpg",\
+       "Григорий",16,70,["v.gd/a","v.gd/b","v.gd/c"]],\
+      ["\/\/upload.wikimedia.org\/wikipedia\/ru\/c\/c9\/Tom_the_Cat.jpg",\
+       "Ирина",17,80,["v.gd/a","v.gd/b","v.gd/c"]],\
+      ["\/\/upload.wikimedia.org\/wikipedia\/ru\/c\/c9\/Tom_the_Cat.jpg",\
+       "Татьяна",18,90,["v.gd/a","v.gd/b","v.gd/c"]],\
+      ["\/\/upload.wikimedia.org\/wikipedia\/ru\/c\/c9\/Tom_the_Cat.jpg",\
+       "Светлана",19,100,["v.gd/a","v.gd/b","v.gd/c"]]]';
 
   data.people.fillRows(jsonData);
   data.people.createTableFromRows();
